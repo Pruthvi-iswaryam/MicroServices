27deb7b2e9ac5ee89df111db23abd505544591fa
Admin <Admin@DESKTOP-E001HR9.mshome.net>
2022-08-23 23:27:55 +0530

    final commit

---
+++ cards/src/main/java/com/eazybytes/cards/model/Customer.java
@@ -0,0 +1,24 @@
+package com.eazybytes.cards.model;
+
+
+
+
+
+public class Customer {
+
+	@Override
+	public String toString() {
+		return "Customer [customerId=" + customerId + "]";
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	private int customerId;
+
+}
